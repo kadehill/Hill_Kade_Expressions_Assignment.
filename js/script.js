6480dcd90036168b7763d61ccacfdf57c98d1da9
kadehill <khill@fullsail.edu>
2015-08-11 18:50:48 -0400

    Definition of Variables & Prompts

--- js/script.js
+++ js/script.js
@@ -2,4 +2,19 @@
  *Kade Hill
  * SDI 1508
  */
-alert("Java works!");
+
+// variables
+var pennies;
+var nickles;
+var dimes;
+var quarters;
+
+
+// prompts
+pennies = prompt("We are calculating how much spare change you have in your car. \n Enter the amount of pennies you've found.");
+nickles = prompt("Enter the amount of nickles you've found");
+dimes = prompt("Enter the amount of dimes you've found.");
+quarters = prompt("Enter the amount of quarters you've found.");
+
+// outputs
+
