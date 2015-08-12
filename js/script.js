01ddb7f780b41a4026d683e755fbf0cd67183385
kadehill <khill@fullsail.edu>
2015-08-11 20:50:45 -0400

    Flow Chart & Final Output

--- js/script.js
+++ js/script.js
@@ -22,11 +22,11 @@ var value = [.01,.05,.10,.25];      // The array is of the value of each coin
 
 // expression & outputs
 var total = ((pennies * value[0]) + (nickles * value[1]) + (dimes * value[2]) + (quarters * value[3]));    //multiplying the amount of each coin by its value
-console.log("You found a total amount of" + " " + "$"+total + " " + "in your car.");             //output of the total
+console.log("You found a total amount of" + " " + "$"+total + " " + "in your car.");  //output of the total amount of spare change found in user's car.
 
 
 
-alert("You found a total of" + " " + "$" + total + " " + "in your car.");        //alert of the total
+alert("You found a total of" + " " + "$" + total + " " + "in your car.");        //alert of the total amount of spare change found in user's car.
 
  // test values
 //** I put in 10 pennies, 30 nickles, 3 dimes and 6 quarters and the calculator gave me a total of $3.40.
