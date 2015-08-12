f7754454242775c8da72b5a7ba9f1b35b162bb64
kadehill <khill@fullsail.edu>
2015-08-11 20:01:52 -0400

    Test Values

--- js/script.js
+++ js/script.js
@@ -17,12 +17,18 @@ nickles = prompt("Enter the amount of nickles that you've found");       // defi
 dimes = prompt("Enter the amount of dimes that you've found.");          // defining
 quarters = prompt("Enter the amount of quarters that you've found.");    // defining
 
-// Array
+// array
 var value = [.01,.05,.10,.25];      // The array is of the value of each coin
 
-// Expression
-var total = ((pennies * value[0]) + (nickles * value[1]) + (dimes * value[2]) + (quarters * value[3]));
+// expression & outputs
+var total = ((pennies * value[0]) + (nickles * value[1]) + (dimes * value[2]) + (quarters * value[3]));    //multiplying the amount of each coin by its value
+console.log("You found a total amount of" + " " + "$"+total + " " + "in your car.");             //output of the total
 
 
-// outputs
-alert("You found a total of" + " " + "$" + total + " " + "in your car.");
+
+alert("You found a total of" + " " + "$" + total + " " + "in your car.");        //alert of the total
+
+ // test values
+//** I put in 10 pennies, 30 nickles, 3 dimes and 6 quarters and the calculator gave me a total of $3.40.
+//* I put in 25 pennies, 0 nickles, 70 dimes and 2 quarters and the calculator gave me a total of $7.75.
+//* I put in 300 pennies, 78 nickles, 40 dimes, and 60 quarters and the calculator gave me a total of  $25.90
